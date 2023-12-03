9079ddf6f1c6226570b8c6df862046144736c0f1
yashika23Grazitti <149145258+yashika23Grazitti@users.noreply.github.com>
2023-12-03 11:49:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+# Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+API_KEY = 'YOUR_API_KEY'
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        city_name = request.form['city']
+        weather_data = get_weather_data(city_name)
+        return render_template('index.html', weather_data=weather_data)
+    return render_template('index.html', weather_data=None)
+
+def get_weather_data(city):
+    params = {'q': city, 'appid': API_KEY, 'units': 'metric'}
+    response = requests.get(BASE_URL, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        weather = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon']
+        }
+        return weather
+    else:
+        return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
